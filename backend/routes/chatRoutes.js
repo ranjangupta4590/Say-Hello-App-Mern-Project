a71a46f3ecee4b9b6d14322a389f4319a97ca940
ranjan_4590 <ranjangupta35558@gmail.com>
2023-01-02 12:28:31 +0530

    fetchchat

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,15 @@
 const express=require('express');
-const {accesschat}=require('../controllers/chatControllers');
+const {accesschat,fetchChats,
+    createGroupChat,
+    renameGroup,
+    addToGroup,
+    removeFromGroup}=require('../controllers/chatControllers');
 const {protect}=require('../middleware/authMiddleware');
 
 const router=express.Router();
 
 router.route('/').post(protect,accesschat);
-// router.route('/').post(protect,fetchchat);
+router.route('/').post(protect,fetchChats);
 // router.route('/group').post(protect,createGroupchat);
 // router.route('/rename').post(protect,renameGroup);
 // router.route('/groupremove').post(protect,removeFromGroup);
